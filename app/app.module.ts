465bbd49c6e459077d6e1f50fd09a90d85881568
Koji Nakagawa and Stella Huayhuaca <koji+stella@>
2017-03-29 11:47:49 -0700

    update

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { EditTaskComponent } from './edit-task.component';
+import { NewTaskComponent } from './new-task.component';
+import { TaskListComponent } from './task-list.component';
 import { FormsModule } from '@angular/forms';
+import { CompletenessPipe } from './completeness.pipe';
 
 @NgModule({
   imports: [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, EditTaskComponent, NewTaskComponent, TaskListComponent, CompletenessPipe],
   bootstrap: [ AppComponent ]
 })
 
---
+++ app/task-list.component.ts
@@ -0,0 +1,59 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from './task.model';
+
+@Component({
+  selector: 'task-list',
+  template:`
+  <select (change)="onChange($event.target.value)">
+    <option value="allTasks">Al Tasks</option>
+    <option value="completedTasks">Completed Tasks</option>
+    <option value="incompleteTasks" selected="selected">Incompleted Tasks</option>
+  <ul>
+    <li>Hello</li>
+    <li (click)="isDone(currentTask)" *ngFor="let currentTask of childTaskList | completeness:filterByCompleteness">{{currentTask.description}}
+    <input *ngIf="currentTask.done === true" type="checkbox" checked (click)="toggleDone(currentTask, false)"/>
+    <input *ngIf="currentTask.done === false" type="checkbox" (click)="toggleDone(currentTask, true)"/>
+    <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button></li>
+  </ul>
+
+  `
+})
+
+
+export class TaskListComponent {
+  @Input() childTaskList: Task[];
+  @Output() clickSender = new EventEmitter();
+  filterByCompleteness: string = "incompleteTasks";
+
+
+  editButtonHasBeenClicked(taskToEdit: Task) {
+    this.clickSender.emit(taskToEdit);
+  }
+
+  isDone(clickedTask: Task ) {
+    if(clickedTask.done === true) {
+      alert("This task is done!");
+    }else {
+      alert("This task is not done. Better get to work");
+    }
+  }
+
+
+  priorityColor(currentTask){
+    if(currentTask.priority === 3){
+      return "bg-danger";
+    }else if (currentTask.priority === 2) {
+      return "bg-warning";
+    }else {
+      return "bg-info";
+    }
+  }
+  onChange(optionFromMenu){
+    this.filterByCompleteness = optionFromMenu;
+  }
+
+  toggleDone(clickedTask: Task, setCompleteness: boolean) {
+    clickedTask.done = setCompleteness;
+  }
+
+}
